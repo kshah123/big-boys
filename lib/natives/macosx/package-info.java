881aba19cba8e94b7171f79c704c1a7f471df57b
Nikolay Pomytkin <nikolayp2800@gmail.com>
2016-05-25 20:15:33 -0400

    bunch of stuff- fuck u moss

--- lib/natives/macosx/package-info.java
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright LWJGL. All rights reserved.
- * License terms: http://lwjgl.org/license.php
- * MACHINE GENERATED FILE, DO NOT EDIT
- */
-
-/** Contains bindings to native APIs specific to the Mac OS X operating system. */
-package org.lwjgl.system.macosx;
-
--- lib/natives/windows/package-info.java
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright LWJGL. All rights reserved.
- * License terms: http://lwjgl.org/license.php
- * MACHINE GENERATED FILE, DO NOT EDIT
- */
-
-/** Contains bindings to native APIs specific to the Windows operating system. */
-package org.lwjgl.system.windows;
-
