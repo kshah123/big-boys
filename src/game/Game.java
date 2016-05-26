972cf73934c219d1d43b069c30d7ff6487c57d01
Nikolay Pomytkin <nikolayp2800@gmail.com>
2016-05-26 06:45:45 -0400

    get rekt jon snoss

--- src/game/Game.java
+++ src/game/Game.java
@@ -1,7 +1,35 @@
 package game;
 
-public class Game {
+import org.newdawn.slick.*;
+import org.newdawn.slick.state.*;
+import org.lwjgl.input.*;
+
+public class Game extends StateBasedGame{
+	
+	public static final String gameName = "Big Boys Video Game";
+	public static final int menu = 0;
+	public static final int play = 1;
+	
+	public Game(String gameName) throws SlickException{
+		super(gameName);
+		this.addState(new Menu(menu));
+		this.addState(new Play(play));
+	}
+	
+	public void initStatesList(GameContainer gc) throws SlickException {
+		this.getState(menu).init(gc, this);
+		this.getState(play).init(gc, this);
+		this.enterState(menu);
+	}
+	
 	public static void main(String[] args) {
-		
+		AppGameContainer appGC;
+		try {
+			appGC = new AppGameContainer(new Game(gameName));
+			appGC.setDisplayMode(appGC.getScreenWidth(), appGC.getScreenHeight(), true);;
+			appGC.start();
+		} catch(SlickException e){
+			e.printStackTrace();
+		}
 	}
 }
--- src/game/Menu.java
+++ src/game/Menu.java
@@ -2,22 +2,32 @@ package game;
 
 import org.newdawn.slick.*;
 import org.newdawn.slick.state.*;
+import org.lwjgl.input.Mouse;
 
 public class Menu extends BasicGameState{
 	
-	public Menu(int state){
-		
+	Image startButton;
+	Image creditsButton;
+	Image helpButton;
+	Image settingsButton;
+	Image exitButton;
+	
+	public Menu(int state) throws SlickException{
+
 	}
 	
 	public void init(GameContainer gc, StateBasedGame sbg) throws SlickException {
-		
+		startButton = new Image("res/btn/startButton.png");
+		creditsButton = new Image("res/btn/creditsButton.png");
+		helpButton = new Image("res/btn/helpButton.png");
+		exitButton = new Image("res/btn/exitButton.jpg");
 	}
 
-	public void render(GameContainer gc, StateBasedGame sbg, Graphics gfx) throws SlickException {
-		
+	public void render(GameContainer gc, StateBasedGame sbg, Graphics g) throws SlickException {
 	}
 
 	public void update(GameContainer gc, StateBasedGame sbg, int delta) throws SlickException {
+
 	}
 
 	public int getID() {
